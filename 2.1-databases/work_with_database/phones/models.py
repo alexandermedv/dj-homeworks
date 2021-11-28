050a0ef24b218440a4b0afa8f06ddefb46dcb612
alexandermed <alexandermed@mail.ru>
2021-11-28 04:10:44 +0300

    task 2.1 is done

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
+    id = models.TextField(primary_key=True)
+    name = models.TextField()
+    price = models.TextField()
+    image = models.TextField()
+    release_date = models.TextField()
+    lte_exists = models.TextField()
+    slug = models.TextField()
     pass
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .models import Phone
 
 
 def index(request):
@@ -7,11 +8,22 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    phones1 = list(Phone.objects.values())
+    sorting = request.GET.get('sort')
+    print(sorting)
+    if sorting == 'name':
+        phones = sorted(phones1, key=lambda d: d['name'])
+    elif sorting == 'min_price':
+        phones = sorted(phones1, key=lambda d: d['price'])
+    elif sorting == 'max_price':
+        phones = sorted(phones1, key=lambda d: d['price'], reverse=True)
+    context = {'phones': phones}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.get(slug=slug)
+    print(phone)
+    context = {'phone': phone}
     return render(request, template, context)
