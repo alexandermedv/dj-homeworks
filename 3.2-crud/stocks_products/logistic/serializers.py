e5804f0dd4e50c4fbd90d4a8c799135cfaf722d9
Alexander Medvedev <medvedevae@pgkweb.ru>
2022-01-13 18:14:35 +0300

    Done according to comments

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -56,6 +56,6 @@ class StockSerializer(serializers.ModelSerializer):
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
         for pos in positions:
-            pos_create = StockProduct.objects.filter(stock_id=instance.id, product_id=pos['product']).update(stock_id=stock.id, product_id=pos['product'].id, quantity=pos['quantity'], price=pos['price'])
+            pos_create, created = StockProduct.objects.filter(stock_id=instance.id, product_id=pos['product']).update_or_create(stock_id=stock.id, product_id=pos['product'].id, quantity=pos['quantity'], price=pos['price'])
 
         return stock
--- 3.2-crud/stocks_products/stocks_products/settings.py
+++ 3.2-crud/stocks_products/stocks_products/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'django_filters',
 
     'logistic',
 ]
@@ -132,4 +133,4 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REST_FRAMEWORK = {'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination','PAGE_SIZE': 5}
+REST_FRAMEWORK = {'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination', 'PAGE_SIZE': 5}
