700f4048e76823a394bd8eb67c9d288e212063d2
alexandermed <alexandermed@mail.ru>
2022-01-15 02:18:24 +0300

    add ordering

--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
-
+import operator
 from articles.models import Article
 
 
 def articles_list(request):
     template = 'articles/news.html'
-    articles = Article.objects.all()
-    print('articles:', articles)
-    # context = {}
-    # for article in articles:
-    #     scopes = article.scopes.all()
-    #     print(scopes)
-    #     for scope in scopes:
-    #         # print(scope.is_main)
-    #         print(scope.tag.name)
-    for article in articles:
-        print('article:', article)
-        print('article.scopes:', article.scopes)
-        print('article.scopes.all:', article.scopes.all())
-        for scope in article.scopes.all():
-            print('scope.tag.name:', scope.tag.name)
+    articles = Article.objects.all().order_by('-published_at')
     context = {'object_list': articles}
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
